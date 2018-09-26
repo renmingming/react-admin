ed01d8dced8e7a53bc6778781c677d60b1bd0bf3
renmingming <renmingming@iyich.com>
2018-09-26 21:24:13 +0800

    no message

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,7 @@
 import JsonP from 'jsonp'
+import axios from 'axios'
+import {Modal} from 'antd'
+
 export default class Axios{
     static jsonp(option) {
         return new Promise((resolve, reject) => {
@@ -9,6 +12,41 @@ export default class Axios{
                     resolve(response);
                 }else{
                     reject(response.message);
+                }
+            })
+        })
+    }
+    static ajax(options) {
+        let loading;
+        if(options.data && options.data.isShowLoading !== false) {
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display = 'block';
+        }
+        let baseApi = 'https://www.easy-mock.com/mock/5bab71e33567340cc7d3dc66/rmmapi';
+        return new Promise((resovle, reject) => {
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: baseApi,
+                timeout: 5000, // 5s超时
+                params: (options.data && options.data.params) || ''
+            }).then((response) => {
+                if(options.data && options.data.isShowLoading !== false) {
+                    loading = document.getElementById('ajaxLoading');
+                    loading.style.display = 'none';
+                }
+                if(response.status === 200) {
+                    let res = response.data;
+                    if(res.code === 0) {
+                        resovle(res)
+                    }else{
+                        Modal.info({
+                            title: '提示',
+                            content: res.msg
+                        })
+                    }
+                }else{
+                    reject(response.data);
                 }
             })
         })
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Card, Table} from 'antd'
-import axios from 'axios'
+import {Card, Table, Modal} from 'antd'
+import axios from './../../axios/index.js'
 
 export default class BasicTable extends React.Component{
     componentWillMount() {
@@ -25,16 +25,42 @@ export default class BasicTable extends React.Component{
         this.request();
     }
     request = () => {
-        let baseUrl = 'https://www.easy-mock.com/mock/5bab71e33567340cc7d3dc66/rmmapi';
-        axios.get(baseUrl + '/table/list').then((res) => {
-            if(res.status === 200) {
+        axios.ajax({
+            url: '/table/list',
+            data: {
+                params: {
+                    page: 1,
+                },
+                isShowLoading: false
+            }
+        }).then((res) => {
+            if(res.code === 0){
+                res.result.list.map((item,index) => {
+                    return item.key = index
+                })
                 this.setState({
-                    dataSource2: res.data.result.list
+                    dataSource2: res.result.list
                 })
             }
         })
     }
+    onRowClick = (record, index) => {
+        // 点击航事件
+        let selectKey = [index]; // 数组可能多选
+        Modal.info({
+            title: `信息`,
+            content: `用户名：${record.userName}`
+        })
+        this.setState({
+            selectEdRowKeys: selectKey, // 行索引
+            selectedItem: record // 行信息
+        })
+    }
     render() {
+        const rowSelection = {
+            type: 'radio',
+            selectEdRowKeys: this.state.selectEdRowKeys
+        }
         const columns = [
             {
                 title: 'id',
@@ -50,21 +76,47 @@ export default class BasicTable extends React.Component{
                 title: '性别',
                 dataIndex: 'sex',
                 key: 'sex',
+                render(sex) {
+                    return sex === 1 ? '男' : '女'
+                }
             },
             {
                 title: '状态',
                 dataIndex: 'state',
                 key: 'state',
+                render(state) {
+                    let config = {
+                        "1": "咸鱼一个",
+                        "2": "风华浪子",
+                        "3": "北大才子",
+                        "4": "百度FE",
+                        "5": "创业者"
+                    }
+                    return config[state]
+                }
             },
             {
                 title: '爱好',
                 dataIndex: 'interest',
                 key: 'interest',
+                render(state) {
+                    let config = {
+                        "1": "打篮球",
+                        "2": "踢足球",
+                        "3": "羽毛球",
+                        "4": "电竞",
+                        "5": "浪"
+                    }
+                    return config[state]
+                }
             },
             {
                 title: '是否已婚',
                 dataIndex: 'isMarried',
                 key: 'isMarried',
+                render(state) {
+                    return state === 1 ? '是' : '否'
+                }
             },
             {
                 title: '生日',
@@ -90,10 +142,26 @@ export default class BasicTable extends React.Component{
                         dataSource={this.state.dataSource}
                     />
                 </Card>
-                <Card title="动态数据渲染表格">
+                <Card title="Mock-动态数据渲染表格">
                     <Table 
                         columns={columns}
                         dataSource={this.state.dataSource2}
+                    />
+                </Card>
+                <Card title="Mock-单选">
+                    <Table 
+                        // 单选或多选
+                        rowSelection={rowSelection}
+                        onRow={(record, index) => {
+                            return {
+                                onClick: () => { // 点击行
+                                    this.onRowClick(record, index)
+                                }
+                            }
+                        }}
+                        columns={columns}
+                        dataSource={this.state.dataSource2}
+                        pagination={false}
                     />
                 </Card>
             </div>
